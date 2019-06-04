59b796e44c9c4f38128a272ed23dc53408c6f7ed
Simon Bitton <simon.bitton1@live.biu.ac.il>
2019-06-04 17:28:55 +0300

    fix command class from last assignment to match this assignment's needs

---
+++ WebApplication3.1/Models/Command.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Web;
+
+namespace WebApplication3._1.Models
+{
+    public class Command
+    {
+        /// <summary>
+        /// singleton design pattern - we want multiple users of the same instance
+        /// </summary>
+        private static Command c_Instance = null;
+        private TcpClient server;
+        public static Command Instance
+        {
+            get
+            {
+                if (c_Instance == null)
+                {
+                    c_Instance = new Command();
+                }
+                return c_Instance;
+            }
+        }
+
+        /// <summary>
+        /// ctor
+        /// </summary>
+        /// <param name="input">the command to be sent</param>
+        /// <param name="flightServerIP">the ip address of the server - flightgear simulator </param>
+        /// <param name="flightCommandPort">the port in which we will open a socket connection </param>
+
+        public Command()
+        {
+            Start();
+        }
+
+        /// <summary>
+        /// the actual connection function
+        /// when invoked - it will send to the server the actual command
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="flightServerIP"></param>
+        /// <param name="flightCommandPort"></param>
+
+        public void Start()
+        {
+            string flightServerIP = "127.0.0.1";
+            int flightCommandPort = 5400;
+            byte[] data = new byte[1024];
+
+            try
+            {
+                server = new TcpClient(flightServerIP, flightCommandPort);
+            }
+            catch (SocketException)
+            {
+                Console.WriteLine("Unable to connect to server");
+                return;
+            }
+        }
+
+        public string[] SendCommand()
+        {
+            NetworkStream ns = server.GetStream();
+            string[] cmds = { "get /position/longitude-deg", "get /position/latitude-deg" };
+            foreach (string cmd in cmds)
+            {
+                string tmpCmd = cmd + "\r\n";
+                ns.Write(Encoding.ASCII.GetBytes(tmpCmd), 0, tmpCmd.Length);
+                ns.Flush();
+            }
+            byte[] msg = new byte[1024];     // byte array
+            ns.Read(msg, 0, msg.Length);   //the networkstream now reads what is being sent from the client
+            char[] charsToTrim = { ' ', '?' };
+            string phrase = Encoding.Default.GetString(msg).Trim(charsToTrim);
+            //Console.WriteLine(phrase); //we print the filtered input from the client
+            cmds = phrase.Split(',', '\n');
+            cmds[0] = cmds[0].Replace("/position/longitude-deg = '", "");
+            cmds[0] = cmds[0].Replace("' (double)\r", "");
+            cmds[1] = cmds[1].Replace("/> /position/latitude-deg = '", "");
+            cmds[1] = cmds[1].Replace("' (double)\r", "");
+            ns.Close();
+            return cmds;
+        }
+
+        public void CloseServer()
+        {
+            server.Close();
+        }
+    }
+}
